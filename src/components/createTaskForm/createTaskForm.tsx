f5afe2e45e15bfcdd0460ec5d7380ff4c7361642
tribagus <tri.bagus21@gmail.com>
2023-01-07 22:45:35 +0700

    add select field

--- src/components/createTaskForm/createTaskForm.tsx
+++ src/components/createTaskForm/createTaskForm.tsx
@@ -1,8 +1,11 @@
 import React, { FC, ReactElement } from 'react';
 import { Box, Typography, Stack } from '@mui/material';
-import { TaskTitleField } from './_taskTitleField';
-import { TaskDescriptionField } from './_taskDescriptionField';
+import { Priority } from './enums/Priority';
+import { Status } from './enums/Status';
 import { TaskDateField } from './_taskDateField';
+import { TaskDescriptionField } from './_taskDescriptionField';
+import { TaskSelectField } from './_taskSelectField';
+import { TaskTitleField } from './_taskTitleField';
 
 export const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -21,6 +24,45 @@ export const CreateTaskForm: FC = (): ReactElement => {
         <TaskTitleField />
         <TaskDescriptionField />
         <TaskDateField />
+
+        <Stack
+          sx={{ width: '100%' }}
+          direction="row"
+          spacing={2}
+        >
+          <TaskSelectField
+            label="Status"
+            name="status"
+            items={[
+              {
+                value: Status.todo,
+                label: Status.todo.toUpperCase(),
+              },
+              {
+                value: Status.inProgress,
+                label: Status.inProgress.toUpperCase(),
+              },
+            ]}
+          />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            items={[
+              {
+                value: Priority.low,
+                label: Priority.low,
+              },
+              {
+                value: Priority.normal,
+                label: Priority.normal,
+              },
+              {
+                value: Priority.high,
+                label: Priority.high,
+              },
+            ]}
+          />
+        </Stack>
       </Stack>
     </Box>
   );
