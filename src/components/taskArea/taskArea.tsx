c4a16f77083f7ff9589ac94629e11397197eed42
tribagus <tri.bagus21@gmail.com>
2023-01-08 11:51:59 +0700

    create task counter dynamic

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,10 +1,47 @@
 import React, { FC, ReactElement } from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
+import { format } from 'date-fns';
+import { TaskCounter } from '../taskCounter/taskCounter';
 
 export const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>content</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP ')}
+        </h2>
+      </Box>
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter />
+          <TaskCounter />
+          <TaskCounter />
+        </Grid>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          md={8}
+        >
+          <Box>Task will come over here</Box>
+          <Box>Task will come over here</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
