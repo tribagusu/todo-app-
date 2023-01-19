1a5dbb6c0e4bf06e3d5d16157d572529673486e0
tribagus <tri.bagus21@gmail.com>
2023-01-19 22:20:06 +0700

    update task with context

--- src/components/taskArea/taskArea.tsx
+++ src/components/taskArea/taskArea.tsx
@@ -1,4 +1,9 @@
-import React, { FC, ReactElement } from 'react';
+import React, {
+  FC,
+  ReactElement,
+  useContext,
+  useEffect,
+} from 'react';
 import {
   Grid,
   Box,
@@ -17,8 +22,13 @@ import { ITaskApi } from './interfaces/ITaskApi';
 import { Status } from '../createTaskForm/enums/Status';
 import { IUpdateTask } from '../createTaskForm/interfaces/IUpdateTask';
 import { countTasks } from './helpers/countTasks';
+import { TaskStatusChangedContext } from '../../context';
 
 export const TaskArea: FC = (): ReactElement => {
+  const tasksUpdatedContext = useContext(
+    TaskStatusChangedContext,
+  );
+
   const { error, isLoading, data, refetch } = useQuery({
     queryKey: ['tasks'],
     queryFn: async () => {
@@ -38,6 +48,16 @@ export const TaskArea: FC = (): ReactElement => {
         data,
       ),
   );
+
+  useEffect(() => {
+    refetch();
+  }, [tasksUpdatedContext.updated]);
+
+  useEffect(() => {
+    if (updateTaskMutation.isSuccess) {
+      tasksUpdatedContext.toggle();
+    }
+  }, [updateTaskMutation.isSuccess]);
 
   const onStatusChangeHandler = (
     e: React.ChangeEvent<HTMLInputElement>,
